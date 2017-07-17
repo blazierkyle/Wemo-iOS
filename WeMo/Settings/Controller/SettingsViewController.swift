be5ef3709fd932cd9cd38aa01839befbafbb2897
Kyle Blazier <blazierkyle@gmail.com>
2017-07-16 22:23:15 -0400

    - Added overlay on map to show geofence

--- WeMo/Settings/Controller/SettingsViewController.swift
+++ WeMo/Settings/Controller/SettingsViewController.swift
@@ -24,6 +24,8 @@ class SettingsViewController: UIViewController {
     
     var geofenceCenterPlacemark: MKPlacemark?
     
+    var geofenceOverlay: MKCircle?
+    
     var dataChanged = false {
         didSet {
             saveButton.isEnabled = dataChanged
@@ -33,7 +35,10 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Set the controller as the MapView delegate
+        map.delegate = self
+        
         // Set current point, radius and time on
         let placemark = MKPlacemark(coordinate: Constants.geofenceCenterCoordinate)
         
@@ -73,11 +78,40 @@ class SettingsViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         
+        setupMapOverlay()
+    }
+    
+    func addGeofenceToMap() {
+        
+        // Remove existing overlays
+        if let existingOverlay = geofenceOverlay {
+            map.removeOverlays([existingOverlay])
+        }
+        
+        // Add the circle overlay to the map to show geofence radius
+        guard let geofenceCenterPlacemark = geofenceCenterPlacemark else {return}
+        
+        let circle = MKCircle(center: geofenceCenterPlacemark.coordinate, radius: Constants.geofenceRadius)
+        map.add(circle)
+        
+        geofenceOverlay = circle
+    }
+    
+    func setupMapOverlay() {
+        
+        addGeofenceToMap()
+        
         // Center the map and zoom it
         map.setCenter(Constants.geofenceCenterCoordinate, animated: false)
         
-        guard let geofenceCenterPlacemark = geofenceCenterPlacemark else {return}
-        map.showAnnotations([geofenceCenterPlacemark], animated: true)
+        // If we have an overlay drawn, show that annotation
+        if let geofenceOverlay = geofenceOverlay {
+            map.setVisibleMapRect(geofenceOverlay.boundingMapRect, animated: true)
+        } else {
+            guard let geofenceCenterPlacemark = geofenceCenterPlacemark else {return}
+            map.showAnnotations([geofenceCenterPlacemark], animated: true)
+        }
+        
     }
     
     func handleLongPress(gestureRecognizer: UILongPressGestureRecognizer) {
@@ -85,10 +119,10 @@ class SettingsViewController: UIViewController {
         // Make sure the gesture is beginning
         guard gestureRecognizer.state == .began else {return}
         
-            // Get the coordinate being long pressed
-            let touchPoint = gestureRecognizer.location(in: self.map)
-            
-            let coordinate = self.map.convert(touchPoint, toCoordinateFrom: self.map)
+        // Get the coordinate being long pressed
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = self.map.convert(touchPoint, toCoordinateFrom: self.map)
         
         // Ask for confirmation
         presentAlert(alertTitle: "Update Geofence Center?", alertMessage: "Are you sure you'd like to update your geofence center and remove the old location?", cancelButtonTitle: "Cancel", cancelButtonAction: nil, okButtonTitle: "Yes, update location", okButtonAction: {
@@ -106,27 +140,33 @@ class SettingsViewController: UIViewController {
                 
                 self.geofenceCenterPlacemark = newGeofencePlacemark
                 
+                // Re-draw geofence radius and re-center map
+                self.setupMapOverlay()
+                
             }
             
             self.dataChanged = true
             
         })
-    
+        
     }
     
     func setDataWasChanged() {
         dataChanged = true
     }
-
+    
     
     @IBAction func saveButtonPressed(_ sender: Any) {
         
+        // Dismiss keyboard
+        view.endEditing(true)
+        
         // Ask for confirmation
-        presentAlert(alertTitle: "Update all fields?", alertMessage: "Are you sure you want to update all fields?", cancelButtonTitle: "Cancel", cancelButtonAction: nil, okButtonTitle: "Yes, change fields", okButtonAction: { 
+        presentAlert(alertTitle: "Update all fields?", alertMessage: "Are you sure you want to update all fields?", cancelButtonTitle: "Cancel", cancelButtonAction: nil, okButtonTitle: "Yes, change fields", okButtonAction: {
             
             // Get required values
             guard let radiusText = self.radiusTextField.text, let radius = Double(radiusText), let turnOnText = self.turnOnTimeTextField.text, let turnOnTime = Double(turnOnText), let point = self.geofenceCenterPlacemark?.coordinate else {
-            
+                
                 self.presentAlert(alertTitle: "Error", alertMessage: "Mising required fields")
                 
                 return
@@ -147,14 +187,25 @@ class SettingsViewController: UIViewController {
             // Disable save button
             DispatchQueue.main.async {
                 self.saveButton.isEnabled = false
+                
+                // Re-draw geofence radius and re-center map
+                self.setupMapOverlay()
             }
             
         })
         
     }
-
+    
 }
 
 extension SettingsViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let circle = overlay as? MKCircle else {return MKOverlayRenderer(overlay: overlay)}
+        let circleRenderer = MKCircleRenderer(circle: circle)
+        circleRenderer.fillColor = UIColor(hexString: "#306EFF")
+        circleRenderer.alpha = 0.4
+        return circleRenderer
+    }
     
 }
